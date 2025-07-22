0ebaa30649ad0e0b5d7a3efd096721c3fe9b53ae
Maple <wjxa20152015@gmail.com>
2025-07-22 13:01:56 +0800

    bump version to 2025.7.22.0

--- kapybara/browserlib/custom_browser.py
+++ kapybara/browserlib/custom_browser.py
@@ -5,14 +5,20 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.remote.webelement import WebElement
 from typing import Optional
-# 初始化查询次数计数器
-query_count = 0
+from pathlib import Path
+import logging
 
 
 class CustomBrowser(webdriver.Chrome):
     def __init__(self, disable_image=False):
+        self.disable_image = disable_image
+
+        if not Path("配置文件/config.ini").exists():
+            logging.critical("配置文件/config.ini 不存在，请检查配置文件路径.")
+            exit(-1)
         # 读取配置文件
         config = configparser.ConfigParser()
+
         config.read('配置文件/config.ini', encoding='utf-8')
 
         # 获取配置文件的内容
@@ -21,41 +27,7 @@ class CustomBrowser(webdriver.Chrome):
 
         if google == 'BIN':
             if browser_drivers == 'no':
-                driver_path = r"BIN\109.0.5414.172\chromedriver.exe"
-                browser_path = r'BIN\thorium.exe'
-                service = Service(driver_path)
-                options = webdriver.ChromeOptions()
-                options.binary_location = browser_path
-                options.add_argument('-ignore-certificate-errors')
-                options.add_argument('-ignore-ssl-errors')
-                options.add_argument("--disable-blink-features=AutomationControlled")
-                options.add_experimental_option('excludeSwitches', ['enable-automation'])
-
-                # # 中文语言
-                # options.add_argument("--lang=zh-CN")
-                # # 用户目录
-                # from pathlib import Path
-                # temp_user_data_dir = Path("chrome_data")
-                # temp_user_data_dir.mkdir(exist_ok=True)
-                # options.add_argument(f"user-data-dir={temp_user_data_dir}")
-
-                if disable_image:
-                    options.add_argument("--blink-settings=imagesEnabled=false")
-                    # TODO KEY 冲突
-                    # prefs = {"profile.managed_default_content_settings.images": 2}
-                    # options.add_experimental_option("prefs", prefs)
-
-                # 方法一：禁用密码保存提示（推荐）
-                # 这会禁止浏览器弹出“保存密码”的提示
-                options.add_experimental_option("prefs", {
-                    "credentials_enable_service": False,
-                    "profile.password_manager_enabled": False
-                })
-
-                # 调用父类（webdriver.Chrome）的构造器
-                super().__init__(service=service, options=options)
-                self.set_page_load_timeout(600)  # 页面加载超时
-                self.implicitly_wait(10)  # 元素未找到时的等待时间
+                self.use_bin_and_driver()
         elif google == 'google':
             if browser_drivers == 'no':
                 super().__init__()
@@ -106,6 +78,55 @@ class CustomBrowser(webdriver.Chrome):
 
     def stop(self):
         self.quit()
+
+    def use_bin_and_driver(self):
+        version = self.find_bin_version()
+        driver_path = f"BIN/{version}/chromedriver.exe"
+        browser_path = r'BIN/thorium.exe'
+        service = Service(driver_path)
+        options = webdriver.ChromeOptions()
+        options.binary_location = browser_path
+        options.add_argument('-ignore-certificate-errors')
+        options.add_argument('-ignore-ssl-errors')
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_experimental_option('excludeSwitches', ['enable-automation'])
+
+        # # 中文语言
+        # options.add_argument("--lang=zh-CN")
+        # # 用户目录
+        # from pathlib import Path
+        # temp_user_data_dir = Path("chrome_data")
+        # temp_user_data_dir.mkdir(exist_ok=True)
+        # options.add_argument(f"user-data-dir={temp_user_data_dir}")
+
+        if self.disable_image:
+            options.add_argument("--blink-settings=imagesEnabled=false")
+            # TODO KEY 冲突
+            # prefs = {"profile.managed_default_content_settings.images": 2}
+            # options.add_experimental_option("prefs", prefs)
+
+        # 方法一：禁用密码保存提示（推荐）
+        # 这会禁止浏览器弹出“保存密码”的提示
+        options.add_experimental_option("prefs", {
+            "credentials_enable_service": False,
+            "profile.password_manager_enabled": False
+        })
+
+        # 调用父类（webdriver.Chrome）的构造器
+        super().__init__(service=service, options=options)
+        self.set_page_load_timeout(600)  # 页面加载超时
+        self.implicitly_wait(10)  # 元素未找到时的等待时间
+
+    def find_bin_version(self) -> str:
+        entries = Path("BIN").iterdir()
+        versions = [entry.name for entry in entries if entry.is_dir() and entry.name.startswith("1")]
+        if len(versions) == 0:
+            logging.critical("无法在BIN目录中找到浏览器.")
+            exit(-1)
+
+        versions.sort(reverse=True)
+        logging.info(f"找到的浏览器版本: {versions}")
+        return versions[0]
 
     def find_element_cross_iframe(
             self,
